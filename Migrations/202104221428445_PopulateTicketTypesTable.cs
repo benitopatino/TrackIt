9d98911f799a70e2dd37e17ebc12b1c69b22c9ac
Benito Patino <contact@benitopatino.com>
2021-04-22 07:33:58 -0700

    Populated TicketTypes table

---
+++ Migrations/202104221428445_PopulateTicketTypesTable.cs
@@ -0,0 +1,21 @@
+namespace TrackIt.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateTicketTypesTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO TicketTypes (Id, Name) VALUES (1,'Task')");
+            Sql("INSERT INTO TicketTypes (Id, Name) VALUES (2,'Bug')");
+            Sql("INSERT INTO TicketTypes (Id, Name) VALUES (3,'Request')");
+            Sql("INSERT INTO TicketTypes (Id, Name) VALUES (4,'Other')");
+
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}

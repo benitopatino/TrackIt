0ba626eb2ba46e8ce91d086034a943da09941e5f
Benito Patino <contact@benitopatino.com>
2021-04-29 08:02:12 -0700

    Seed ProjectStatus migration

---
+++ Migrations/202104291454459_Seed ProjectStatus.cs
@@ -0,0 +1,20 @@
+﻿namespace TrackIt.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedProjectStatus : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO ProjectStatus (Id, Name) VALUES (1,'New')");
+            Sql("INSERT INTO ProjectStatus (Id, Name) VALUES (2,'On Track')");
+            Sql("INSERT INTO ProjectStatus (Id, Name) VALUES (3,'On Hold')");
+            Sql("INSERT INTO ProjectStatus (Id, Name) VALUES (4,'Completed')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

4748a6212d5a286123e210a27453f7b3bc970c86
Benito Patino <contact@benitopatino.com>
2021-04-30 00:18:21 -0700

    Work on Project List Ticket status count

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrackIt.Models;
+using System.Data.Entity;
+using TrackIt.ViewModels;
+
+namespace TrackIt.Controllers
+{
+    public class ProjectsController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public ProjectsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
+        // GET: Projects
+        public ActionResult Index()
+        {
+            var projects
+         
+            var viewModel = new ProjectListViewModel()
+            {
+                Projects = _context.Projects.ToList(),
+                TicketStatusCount = GetTicketStatusCount
+
+        }
+            return View("List", projects);
+        }
+
+
+        private Dictionary<string, int> GetTicketStatusCount(string projectId)
+        {
+
+            IEnumerable<Status> statusList = _context.Status.ToList();
+            Dictionary<string, int> tempDict = new Dictionary<string, int>();
+
+            foreach(var s in statusList)
+            {
+
+                int count = _context.Tickets.Where(t => t.ProjectId == projectId).ToList().Count;
+                tempDict.Add(s.Name, count);
+            }
+
+            return tempDict;
+
+        }
+    }
+}
---
+++ ViewModels/ProjectListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TrackIt.Models;
+
+namespace TrackIt.ViewModels
+{
+    public class ProjectListViewModel
+    {
+        public IEnumerable<Project> Projects{ get; set; }
+        public Dictionary<string, int> TicketStatusCount { get; set; }
+
+    }
+}

1bfa21b06f823a196f240f56792fefd971bbd5e3
Benito Patino <contact@benitopatino.com>
2021-04-29 20:23:12 -0700

    Added display annotations to Ticket Model

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,19 +12,36 @@ namespace TrackIt.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public ApplicationUser Assignee { get; set; }
+
+        [Display(Name = "Owner")]
         public string AssigneeId { get; set; }
+
         public Priority Priority { get; set; }
+
+        [Display(Name = "Priority")]
         public byte PriorityId { get; set; }
+        
         public Status Status { get; set; }
+
+        [Display(Name = "Priority")]
         public byte StatusId { get; set; }
+        
         public DateTime DateCreated { get; set; }
         public DateTime? DateClosed { get; set; }
         public DateTime? DateDue { get; set; }
         public TicketType TicketType { get; set; }
+
+        [Display(Name = "Ticket Type")]
         public byte TicketTypeId { get; set; }
+        
         public Resolution Resolution { get; set; }
+
+        [Display(Name = "Resolution")]
         public byte? ResolutionId { get; set; }
+        
         public Project Project { get; set; }
+
+        [Display(Name = "Project")]
         public string ProjectId { get; set; }
 
     }

65b864c70437aae6f83379926137a4e62ebbf69c
Benito Patino <contact@benitopatino.com>
2021-05-08 01:09:02 -0700

    Added Required data annotation to Comment model (Content) to prevent empty comments from being posted.

---
+++ Migrations/202105080803214_AddRequiredAnnotationToCommentModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TrackIt.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddRequiredAnnotationToCommentModel : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Comments", "Content", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Comments", "Content", c => c.String());
+        }
+    }
+}
--- Models/Comment.cs
+++ Models/Comment.cs
@@ -8,16 +8,25 @@ namespace TrackIt.Models
 {
     public class Comment
     {
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(string ticketId, string userId)
+        {
+            TicketId = ticketId;
+            AuthorId = userId;
+        }
+
         public string Id { get; set; }
+
+        [Required]
         public string Content { get; set; }
         public ApplicationUser Author { get; set; }
-
-        [Required]
-        public string AuthorId { get; set; }
-
+        public string  AuthorId { get; set; }
         public Ticket Ticket { get; set; }
-
-        [Required]
         public string TicketId { get; set; }
         public DateTime DateCreated { get; set; }
 

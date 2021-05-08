e7d4279847e56f032cfefca4ecafd62b4b07a993
Benito Patino <contact@benitopatino.com>
2021-05-07 19:46:14 -0700

    Add FirstName property to Identity User and performed migration

---
+++ Migrations/202105080242376_AddFirstNamePropertyToIdentityUser.cs
@@ -0,0 +1,18 @@
+﻿namespace TrackIt.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddFirstNamePropertyToIdentityUser : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "FirstName", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "FirstName");
+        }
+    }
+}

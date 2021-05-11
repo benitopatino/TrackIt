e6bf9c287a11463e61d1493b559e92406815ce48
Benito Patino <contact@benitopatino.com>
2021-05-11 10:22:44 -0700

    Add Required Annotation to Project model

--- Models/Project.cs
+++ Models/Project.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations;
 namespace TrackIt.Models
 {
     public class Project
     {
         public string Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public ProjectStatus ProjectStatus { get; set; }
+
+        [Required]
         public byte ProjectStatusId { get; set; }
 
     }

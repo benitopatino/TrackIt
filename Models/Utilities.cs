fd5b3bd1ab3fa879d3093d0fb803796610a78893
Benito Patino <contact@benitopatino.com>
2021-07-16 22:19:46 -0700

    Added ReadOnly & List for Projects

---
+++ Models/Utilities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrackIt.Models
+{
+    public static class Utilities
+    {
+        public static class ProjectStatus
+        {
+            // Project Status
+            public static string Completed = "Completed";
+            public static string OnHold = "On Hold";
+            public static string OnTrack = "On Track";
+            public static string New = "New";
+        }
+
+    }
+}
